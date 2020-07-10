be406be599e266b52beabb8a2c5f46a094daf6d7
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-07-10 14:51:14 -0300

    add validate brand in ProductController

--- appProductReport/src/controller/ProductController.java
+++ appProductReport/src/controller/ProductController.java
@@ -7,6 +7,7 @@ public class ProductController {
     public boolean validate(Product product) {
         return product.getAmount() >= 0
                 && product.getPrice() >= 0
-                && !product.getName().isEmpty();
+                && !product.getName().isEmpty()
+                && !product.getBrand().getName().isEmpty();
     }
 }
