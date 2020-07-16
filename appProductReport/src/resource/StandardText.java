5675099fc0a74078a9a93bf8102bd3b89119746c
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-07-16 16:54:47 -0300

    create standards texts

---
+++ appProductReport/src/resource/StandardText.java
@@ -0,0 +1,19 @@
+package resource;
+
+abstract public class StandardText {
+
+    /*Error*/
+    public static final String ERROR = "Error: ";
+    public static final String TITLE_ERROR = "Erro";
+    public static final String INTERNAL_ERROR = "Erro interno";
+    public static final String BRAND_ALREADY_REGISTERED = "Marca ja cadastrada";
+    public static final String ERROR_FETCHING_RECORDS = "Erro ao buscar registro(s)";
+
+    /*Success*/
+    public static final String SUCCESS = "Sucesso";
+    
+    /*Others*/
+    public static final String OPERATION_STATUS = "Status da operacao";
+    public static final String SELECT = "**Selecione**";
+    public static final String ENTER_BRAND_NAME = "Digite o nome da marca";
+}
