178c92f298098fc52219ab1f5f7129baf8dca99f
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-07-16 17:25:24 -0300

    create construct

--- appProductReport/src/model/Brand.java
+++ appProductReport/src/model/Brand.java
@@ -5,6 +5,14 @@ public class Brand {
     private int id;
     private String name;
 
+    public Brand(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Brand() {
+    }
+
     public int getId() {
         return id;
     }
