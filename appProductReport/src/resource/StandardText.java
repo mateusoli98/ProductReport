859ac3e3d2658e9179b5231519ddb1b4b5db7d07
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-07-18 14:08:05 -0300

    insert messages in StandardText

--- appProductReport/src/resource/StandardText.java
+++ appProductReport/src/resource/StandardText.java
@@ -8,10 +8,12 @@ abstract public class StandardText {
     public static final String INTERNAL_ERROR = "Erro interno";
     public static final String BRAND_ALREADY_REGISTERED = "Marca ja cadastrada";
     public static final String ERROR_FETCHING_RECORDS = "Erro ao buscar registro(s)";
+    public static final String NUMBER_FORMART_EXCEPTION = "Preencha corretamente os campos numericos";
+    public static final String EMPTY_FIELDS = "Por favor, preencha todos os campos";
 
     /*Success*/
     public static final String SUCCESS = "Sucesso";
-    
+
     /*Others*/
     public static final String OPERATION_STATUS = "Status da operacao";
     public static final String SELECT = "**Selecione**";
